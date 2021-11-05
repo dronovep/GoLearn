68a06f6e9caa19e0274f184d31507d97c88ff017
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-05 13:51:56 +0300

    Пример чисел с плавающей запятой

--- hello.go
+++ hello.go
@@ -2,48 +2,36 @@ package main
 
 import "fmt"
 
-func msum(a int, b int) int {
-	return a + b
-}
+var factor1 float64 = 12_76.12_23e-05
+var factor2 float64 = 1.1223e-05
+var factor3 float64 = 12_76.12_23e-08
+var factor4 float64 = 1.1223e-03
+var factor5 float64 = 1.3e+3
+var factor6 float64 = 0x1.p-01
+var factor7 float64 = 0x0.11p0
 
-func mmul(a int, b int) int {
+/** экспоненциальная запись с мантиссой и экспонентой: number = mantissa * base^exponenta
+В случае десятичной системы счисления base = 10, вместо p пишется e
+В случае системы счисления отличной от 10, base = 2, пишется p
 
-	return a * b
-}
-
-var (
-	mother  string = "Irina"
-	father  string = "Pavel"
-	brother string = "Anton"
-	wife    string = "Dasha"
-	son     string = "Cyrill"
-	sson    string = "Alexey"
-)
-
-//var sym rune = '\u0062'
-var code int = 333_157
+mantissa - это все, что идет до "p или e" в соответствующей системе счисления
+*/
 
 func main() {
-	//var m, f, s, b int = 5, 6, 7, 0xf
-
-	//fmt.Printf("Hello dude! m + f = %d, s * b = %d\n", msum(m ,f), mmul(s, b))
-	fmt.Printf("Code = %d", code)
-	//println(m, f, s, b)
-	//fmt.Printf(
-	//	"Hello %s, %s, %s, %s, %s, %s",
-	//		mother,
-	//		father,
-	//		brother,
-	//		wife,
-	//		son,
-	//		sson,
-	//	)
-	//var a int = 3
-	//var b int = 5
-	//var p *int = &a
-	//
-	//fmt.Printf("p points to a, *p = %d, ", *p)
-	//p = &b
-	//fmt.Printf("p points to b, *p = %d", *p)
-	//fmt.Printf("Blank identifier equals to %d", _)
+	fmt.Printf(
+		"factor1 = %f\n"+
+			"factor2 = %f\n"+
+			"factor3 = %f\n"+
+			"factor4 = %f\n"+
+			"factor5 = %f\n"+
+			"factor6 = %f\n"+
+			"factor7 = %f\n",
+		factor1,
+		factor2,
+		factor3,
+		factor4,
+		factor5,
+		factor6,
+		factor7,
+	)
 }
