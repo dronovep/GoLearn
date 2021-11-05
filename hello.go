26424c22d9209d9048538714d03b6891b780e787
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-05 14:04:25 +0300

    Пример комплексного числа

--- hello.go
+++ hello.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+/**
+Для получения мнимой части комплексного числа просто добавляем "i" к концу вещественного числа
+*/
+
+var (
+	img1 complex64 = 35.5 + 25.3e-4i
+)
+
+func main() {
+	mvar := 35
+	fmt.Printf(
+		"%f, %d",
+		img1,
+		mvar,
+	)
 }
