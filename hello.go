127cedcdcaf517dfdb4d80fecca841e590161c2c
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-05 15:15:01 +0300

    Примеры строковых констант

--- hello.go
+++ hello.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+/** внутри `` - сырые строки
+внутри "" - интерпретируемые строки (в них обрабатываются спецсимволы)
+*/
+
+var (
+	family    = `mother father sister brother\nYes!`
+	family_i  = "mother father sister brother\nYes!"
+	family_r  = `мама папа сестра брат \nДа!`
+	family_ri = "мама папа сестра брат \nДа!"
+	mama      = "mam\u0041\n"
+)
+
+func main() {
+	fmt.Printf("Family is %s, Again Family is %s", family, family_i)
+	fmt.Printf("\n\n\n")
+	fmt.Printf("Семья это %s, Опять семья это %s", family_r, family_ri)
+	fmt.Printf(`Вот это \r`) // проверяем поведение \r в сырой строке
+	fmt.Printf("%s", mama)
 }
