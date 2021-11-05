491b467f08d4766f3ab94ca86f5e5f733ce253dd
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-05 14:36:46 +0300

    Пример символьных констант (rune constants)

--- hello.go
+++ hello.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+var (
+	a       int = 'a'
+	A       int = 'A'
+	newline     = '\n'
+	eof         = '\u0000'
+	uni_A       = '\u0041' // ASCII код символа A - это 65, в юникоде U + 65 в 16ричной системе счисления - U+0041
+)
+
+func main() {
+	fmt.Printf(
+		"%d\n"+
+			"%d\n"+
+			"%d\n"+
+			"%d\n"+
+			"%c\n",
+		a,
+		A,
+		newline,
+		eof,
+		uni_A,
+	)
 }
