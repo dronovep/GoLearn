d2f53f92106603550d0ba38e34fb151189108eb8
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-09 23:52:03 +0300

    Примеры работы с функциями

--- hello.go
+++ hello.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+type rstr func() string
+
+func greet() string {
+	return "Calling greet"
+}
+
+func bye() string {
+	return "Calling bye"
+}
+
+func generate_bm() (brand string, model string) {
+	//func generate_bm() (string, string) {
+	brand = "Kia"
+	model = "Sorento"
+
+	return brand, model
+	//return "Kia", "Sorento"
+}
+
+func printStringArgs(args ...string) {
+	fmt.Printf("\n\nString args are:\n")
+	for i := range args {
+		fmt.Printf("  %s\n", args[i])
+	}
+}
+
+var actions []rstr = []rstr{greet, bye}
+
+func sum(a int, b int) (result int) {
+	result = a + b
+	return 25
+}
+
 func main() {
 
+	for i := 0; i < len(actions); i++ {
+		fmt.Printf("%s\n", actions[i]())
+	}
+
+	a, b := generate_bm()
+
+	fmt.Printf("sum(3, 2) = %d\n", sum(3, 2))
+
+	fmt.Printf("Brand = %s, model = %s\n", a, b)
+
+	printStringArgs("mother", "father", "sister", "brother")
+
+	fmt.Printf("Command arguments are:\n")
+	for i := range os.Args {
+		fmt.Printf("    %s\n", os.Args[i])
+	}
 }
