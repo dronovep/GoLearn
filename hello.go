26588ea8f7bad20cae48bd48aac01c1b8b288d80
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-09 18:35:04 +0300

    Пример структур. Незавершено

--- hello.go
+++ hello.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
+import "math"
 
+type Point struct {
+	x        int
+	y        int
+	distance func() float64
+}
+
+var p Point = Point{3, 2, func() float64 {
+	return math.Sqrt(x*x + y*y)
+}}
+
+func main() {
+	fmt.Printf("Привет мир!\n")
+	fmt.Printf("Точка: x = %d, y = %d\n", p.x, p.y)
 }
