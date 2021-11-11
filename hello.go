c15c0b1f700de54163aeab5071c30bc81ed5031d
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-11 20:01:38 +0300

    Пример мапов

--- hello.go
+++ hello.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	mmap := make(map[string]int, 4)
+
+	mmap["mother"] = 1
+	mmap["father"] = 2
+	mmap["brother"] = 3
+	mmap["me"] = 4
+	mmap["extra"] = 10 //это уже пятый элемент, но расширить видимо можно
+
+	for key := range mmap {
+		fmt.Printf("mmap[%s] = %d\n", key, mmap[key])
+	}
 
 }
