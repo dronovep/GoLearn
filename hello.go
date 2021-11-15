2c4dce9cde0582e435104399e804471f884a0817
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-15 22:16:41 +0300

    Первые наработки замыкания

--- hello.go
+++ hello.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
+func createCounter(init int) func() int {
+	start := init
+
+	return func() int {
+		out := start
+		start += 1
+		return out
+	}
+}
+
 func main() {
+
+	counters := [5]func() int{}
+
+	for i := 0; i < 5; i++ {
+		counters[i] = createCounter(i)
+	}
+
+	for i := 0; i < 5; i++ {
+		for k := 0; k < 3; k++ {
+			fmt.Printf("count = %d\n", counters[i]())
+		}
+		fmt.Printf("next counter\n")
+	}
 
 }
