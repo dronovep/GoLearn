70fc1d5fa87e19f76b9c45e7f02121fdd05761d0
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-21 16:44:58 +0300

    Первые наработки по операторам

--- hello.go
+++ hello.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+var flag bool = true
+var chislo uint8 = 128
+
+func main() {
+	fmt.Printf("flag = %v, !flag = %v\n", flag, !flag)
+	fmt.Printf("chislo = %v, !chislo = %v\n", chislo, ^chislo)
 }
