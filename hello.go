0d8cc3743e0917438a955cdc024029394cff1172
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-22 19:48:35 +0300

    Примеры сравнения строк

--- hello.go
+++ hello.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func printStringComparison(a string, b string) {
+	fmt.Printf("\n%s == %s : %v\n", a, b, a == b)
+	fmt.Printf("%s > %s : %v\n", a, b, a > b)
+	fmt.Printf("%s < %s : %v\n", a, b, a < b)
+}
+
+//Строки можно проверить не тольо на полное равенство, но и на порядок
+//В этом случае строку можно представить как число в системе счисления из одних букв алфавита
+
+func main() {
+	printStringComparison("mother", "father")
+	printStringComparison("a", "b")
+	printStringComparison("aa", "ab")
+	printStringComparison("vaka", "vaka")
+	printStringComparison("str1", "str2")
+	printStringComparison("nab", "aaa")
+	printStringComparison("Привет", "Привет2")
 }
