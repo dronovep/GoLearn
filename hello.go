8ff84d38625f40520ebcac8e651b99b0aa522acb
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-25 01:56:01 +0300

    Еще примеры с указателями и массивами

--- hello.go
+++ hello.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type mp1 = int
 type mp2 = int
 
@@ -61,37 +59,44 @@ func main() {
 
 	//var mbytes  = [...]rune{'a', 'l', 'p', 'h', 'a'}    //можно и так
 	//var mbytes  = [...]byte{'a', 'l', 'p', 'h', 'a'}  // и так
-	var mbytes = [5]rune{'a', 'l', 'p', 'h', 'a'} //можно и так
-	var msl = mbytes[:3]
-	var mstr = string(msl)
-	//var mstr = string(mbytes)					// массив сконвертировать в строку не получится, только слайс можно
-	fmt.Printf("mstr = %s\n", mstr)
 
-	var mstr2 string = "father"
-	var syms []rune = []rune(mstr2)
-	syms[0] = 'm'
-	syms[1] = 'o'
-	fmt.Printf("father : f->m, a->o : %s\n", string(syms))
-	fmt.Printf("sysms: len = %d, cap = %d, value = %v\n", len(syms), cap(syms), syms)
-
-	var mbytes2 = [...]byte{116, 104, 101, 114}
-	var msl2 = mbytes2[:]
-	fmt.Printf("numbers equal to string: %s\n", string(msl2))
-
-	var marr1 = [10]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
-	//var par  = (*[3]int)(&marr1[5]) // делать указатель на кусок массива не получится
-	var mslar = marr1[:]
-	var par = (*[3]int)(mslar)
-	fmt.Printf("par points to %v\n", *par)
-	par = (*[3]int)(marr1[5:])
-	fmt.Printf("par points to %v\n", *par)
-	par[0] = 125
-	fmt.Printf("marr1 = %v\n", marr1)
-
-	var par2 = &par[1] // здесь тип *int
-	fmt.Printf("par2: %v\n", *par2)
+	//var mbytes = [5]rune{'a', 'l', 'p', 'h', 'a'} //можно и так
+	//var msl = mbytes[:3]
+	//var mstr = string(msl)
+	////var mstr = string(mbytes)					// массив сконвертировать в строку не получится, только слайс можно
+	//fmt.Printf("mstr = %s\n", mstr)
+	//
+	//var mstr2 string = "father"
+	//var syms []rune = []rune(mstr2)
+	//syms[0] = 'm'
+	//syms[1] = 'o'
+	//fmt.Printf("father : f->m, a->o : %s\n", string(syms))
+	//fmt.Printf("sysms: len = %d, cap = %d, value = %v\n", len(syms), cap(syms), syms)
+	//
+	//var mbytes2 = [...]byte{116, 104, 101, 114}
+	//var msl2 = mbytes2[:]
+	//fmt.Printf("numbers equal to string: %s\n", string(msl2))
+	//
+	//var marr1 = [10]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	////var par  = (*[3]int)(&marr1[5]) // делать указатель на кусок массива не получится
+	//var mslar = marr1[:]
+	//var par = (*[3]int)(mslar)
+	//fmt.Printf("par points to %v\n", *par)
+	//par = (*[3]int)(marr1[5:])
+	//fmt.Printf("par points to %v\n", *par)
+	//par[0] = 125
+	//fmt.Printf("marr1 = %v\n", marr1)
+	//
+	//var par2 = &par[1] // здесь тип *int
+	//fmt.Printf("par2: %v\n", *par2)
 
 	// Резюме! Из масива можно получить слайс, из слайса можно взять указатель на массив
 	// Из массива получить указатель на подмассив не получится
 	// Из слайса можно получить указатель на подслайс
+
+	var ip = []byte{255, 255, 255, 255}
+	//var ipn = int32(ip) // так не получится
+	var smth interface{} = ip
+	//var ipn int32 = int32(smth) // так тоже не получится
+	var ipn = (*int32)(&ip[0]) // и даже так не получится
 }
