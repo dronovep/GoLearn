54ec462660f0623c701c17dd2c04c6195b315f7e
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-25 22:57:26 +0300

    Первые наработки

--- hello.go
+++ hello.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	i := 2
+	x := []int{3, 5, 7}
+	for i, x[i] = range x {
+		fmt.Printf("i = %d, x[i] = %d\n", i, x[i])
+		break
+	}
+
+	fmt.Printf("i = %d, x = %v\n", i, x)
 }
