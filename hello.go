a2cfafc1e86e8a82b0f3492534fe607f53319844
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-27 00:34:35 +0300

    Пример IFа с проверкой типов

--- hello.go
+++ hello.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	//var a interface{} = 32
+	//var a interface{} = 7.3
+	var a interface{} = true
+
+	if _, isInt := a.(int); isInt {
+		fmt.Printf("Вы задали целое число %d\n", a)
+	} else if _, isFloat := a.(float64); isFloat {
+		fmt.Printf("Вы задали вещественное число %f\n", a)
+	} else {
+		fmt.Printf("Вы задали значение %v\n", a)
+	}
 
 }
