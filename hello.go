343f459c9c9c2732c127fc684f640e234dfb676f
Eugene Dronov <dronov-ep@yandex.ru>
2021-11-26 23:39:35 +0300

    Пример с метками и переходами goto

--- hello.go
+++ hello.go
@@ -1,5 +1,36 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	family := []string{"mother", "father", "brother", "me"}
+	mbuffer := make([]rune, 50, 100)
+	famsize := len(family)
+	i, j, k := 0, 0, 0
+mcycle:
+	if i < famsize {
+		member := family[i]
+		mlen := len(member)
+	lcycle:
+		if j < mlen {
+			letter := member[j]
+			mbuffer[k] = rune(letter)
+			j++
+			k++
+			goto lcycle
+		}
+		j = 0
+		k++
+		mbuffer[k] = ' '
+		k++
+
+		i++
+		goto mcycle
+	}
+
+	k++
+	mbuffer[k] = 0
+
+	fmt.Printf("Result string:\n%s", string(mbuffer))
 }
