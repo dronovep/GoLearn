48cb3f038ea541f6501d8daf51e1040cb01a8ad3
Eugene Dronov <dronov-ep@yandex.ru>
2021-12-02 22:44:05 +0300

    Первый вариант

--- hello.go
+++ hello.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import "fmt"
 
+type Greeter interface {
+	greet()
+}
+
+type AbstractGreeter struct {
+	Greeter
+	name string
+}
+
+func (o *AbstractGreeter) hello() {
+	fmt.Printf("Hello, my friend!\n")
+	o.greet()
+	fmt.Printf("Goodbye\n")
+}
+
+type Dog struct {
+	AbstractGreeter
+}
+
+func (o *Dog) greet() {
+	fmt.Printf("Woof!Woof! My name is %s!\n", o.name)
+}
+
+func createDog(name string) *Dog {
+	dog := new(Dog)
+	dog.name = name
+	dog.Greeter = dog
+	return dog
+}
+
+func main() {
+	var greeter *AbstractGreeter = &createDog("Pongo").AbstractGreeter
+	greeter.hello()
 }
